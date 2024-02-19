94b83653e19da6cd3db1046ae2851c452f98a42e
Marco Antonio <marcoaevaristo@hotmail.com>
2024-02-19 09:09:50 -0300

    Projeto lanca notas

---
+++ Projeto M8/main.js
@@ -0,0 +1,29 @@
+const from = document.getElementById('form-atividade');
+const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji celebrando" />';
+const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji decepcionado" />';
+
+let linhas = '';
+
+from.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const inputNomeAtividade = document.getElementById('nome-atividade');
+    const inputNotaAtividade = document.getElementById('nota-atividade');
+
+
+    let linha = `<tr>`;
+    linha += `<td>${inputNomeAtividade.value}</td>`;
+    linha += `<td>${inputNotaAtividade.value}</td>`;
+    linha += `<td>${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado}</td>`;
+    linha += `</tr>`;
+
+    linhas += linha;
+
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+
+    inputNomeAtividade.value = '';
+    inputNotaAtividade.value = '';
+
+});
+
