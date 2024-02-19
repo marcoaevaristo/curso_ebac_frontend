15b69622d71a1f85672e67a249e79fc80ab6d506
Marco Antonio <marcoaevaristo@hotmail.com>
2024-02-19 14:30:59 -0300

    Criando Agenda

---
+++ Projeto Agenda Telefone/main.js
@@ -0,0 +1,39 @@
+const form = document.getElementById('form-entrada');
+const numeros = [];
+
+let linhas = '';
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+});
+
+function adicionaLinha() {
+    const inputNomeContato = document.getElementById('nome-contato');
+    const inputNumeroContato = document.getElementById('numero-contato');
+
+    console.log(numeros);
+
+    if (numeros.includes(inputNumeroContato.value)) {
+        alert(`O numero: "${inputNumeroContato.value}" ja foi adicionado`);
+    } else {
+        numeros.push(inputNumeroContato.value);
+
+        let linha = '<tr>';
+        linha += `<td>${inputNomeContato.value}</td>`;
+        linha += `<td>${inputNumeroContato.value}</td>`;
+        linha += `</tr>`;
+
+        linhas += linha;
+    }
+
+    inputNomeContato.value = '';
+    inputNumeroContato.value = '';
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
